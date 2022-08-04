0a25ec4dbdb9520bde790cbaf3d866275e241d25
Eddiedeprince <bigplas2@gmail.com>
2022-08-04 03:35:01 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="headpage">
+     <img src="image/ACADIMIC.png" alt="">
+
+    <div class="title">
+
+      <h4>SKYEYE</h4>
+
+    </div>
+     
+
+  <div class="navbar">
+     <a href="#">Home</a>
+     <a href="#">LiveRoom</a>
+     <a href="#">Course</a>
+     <a href="#">Chat</a>
+     <a href="#">AboutUs</a>
+   </div>
+
+    <div class="registartion">
+        <button>Register</button>
+    </div>
+
+    <div class="mainpage">
+        <h2>
+               Fast task for academicals group is to see proficienal of the content.
+               Second task for 
+        </h2>
+            <p>
+              Fast task for academicals group is to see proficienal of the content.
+              Second task for 
+              Fast task for academicals group is to see proficienal of the content.
+              Second task for ast task for academicals group is to see proficienal of the content.
+               ast task for academicals group is to see proficienal of the content.
+            </p>
+    </div>
+
+    <div class="demo">
+
+      <button>View Demo</button>
+    </div>
+    <div class="sticker">
+
+    </div>
+</div>
+<!--our service section started-->
+
+<div class="service">
+  <h1>
+Our service
+  </h1>
+  <p>
+  Fast task for academicals group is to see proficienal of the content.
+              Second task for ast task for academicals group is to see proficienal of the content.
+  </p>
+
+</div>
+
+<div>
+  this just a teach to you seee 
+</div>
+
+</body>
+</html>
